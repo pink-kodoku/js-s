76ce09e8a631ab07cdc2f4167956675de8df13b7
Petia Duskin <petr.dyuskin@mail.ru>
2023-04-25 18:50:15 +0500

    add more about weakMap and weakSet

--- weakMap_weakSet.js
+++ weakMap_weakSet.js
@@ -1,35 +1,48 @@
+let john = { name: "John" };
 
-
-let john = { name: 'John' }
-
-let arr = [john]
+let arr = [john]; // объект john все еще хранится в массиве, так как является достижимым
 
 john = null;
 
-console.log(arr[0])
+console.log(arr[0]); // john object
 
 // WeakMap - позволяет сборщику мусора удалять объекты из памяти
 
 let weakMap = new WeakMap();
 
-let john2 = { name: 'John' }
+let john2 = { name: "John" };
 
-weakMap.set(john2, 'value')
+weakMap.set(john2, "value");
 
 john2 = null;
 
-console.log(weakMap.get(john2))
-
+console.log(weakMap.get(john2)); // undefined, так как объект уже удален сборщиком мусора
 
 // Ключи в WeakMap должны быть только объектами
 
 let weakMap2 = new WeakMap();
-const user = { name: 'petia' };
 
-weakMap2.set(user, 'some value')
+const user = { name: "petia" };
 
-console.log(weakMap2.get(user))
+weakMap2.set(user, "some value");
+
+console.log(weakMap2.get(user));
 
 // В WeakMap отсутствуют методы для перебора значений
 
+// WeakMap удобно использовать для кеширования, например
+
+const cache = new WeakMap();
+
+const proccess = (obj, fn) => {
+    if (!cache.has(obj)) {
+        cache.set(obj, fn(obj))
+    }
+    return cache.get(obj)
+}
+
 // WeakSet - структура данных, в которую мы можем добавлять только объекты
+
+let weakSet = new WeakSet();
+
+// Можем использовать в качестве дополнительных данных
